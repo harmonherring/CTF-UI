898846261ed7be609d9e40d70f63df21570a5c47
Harmon Herring <harmonherring@gmail.com>
2020-05-11 17:11:48 -0500

    Change oidc client id and auth flow

--- src/userManager.js
+++ src/userManager.js
@@ -3,7 +3,7 @@ import { createUserManager } from 'redux-oidc'
 const userManagerConfig = {
   client_id: process.env.REACT_APP_SSO_CLIENT_ID,
   redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/callback`,
-  response_type: 'token id_token',
+  response_type: 'code',
   scope: 'openid profile',
   authority: process.env.REACT_APP_SSO_AUTHORITY,
   silent_redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/silent_renew.html`,
